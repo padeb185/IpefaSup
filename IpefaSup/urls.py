c9df2c15086fbc919f0483873cebebc243bf43c0
padeb185 <pierre-andre@hotmail.be>
2025-03-28 18:08:08 +0100

    welcome

--- IpefaSup/urls.py
+++ IpefaSup/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib.auth import login
 from django.contrib import admin
 from django.urls import path
-
 from IpefaSup.views import welcome
 
 urlpatterns = [
